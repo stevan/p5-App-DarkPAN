06db2652c9434d6d75aad0dcb8f03372510a4135
Stevan Little <stevan.little@booking.com>
2016-10-06 21:11:47 +0200

    generate tables

--- lib/App/DarkPAN/Command.pm
+++ lib/App/DarkPAN/Command.pm
@@ -6,7 +6,8 @@ use warnings;
 our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:STEVAN';
 
-use Path::Tiny ();
+use Path::Tiny        ();
+use Text::Table::Tiny ();
 
 use App::Cmd::Setup -command;
 
@@ -20,6 +21,17 @@ sub opt_spec {
     );
 }
 
+sub generate_data_table {
+    my ($self, $data, $keys) = @_;
+    my @keys = sort $keys ? @$keys : keys %{ $data->[0] };
+    return Text::Table::Tiny::generate_table(
+        header_row => 1,
+        rows       => [
+            \@keys,
+            map [ @{$_}{ @keys } ], @$data,
+        ]
+    );
+}
 
 1;
 
